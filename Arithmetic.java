3e34b0afe54c8bf4f58d3756d08762e1fcfb5652
Solomon Tsehay <86978444+mikej-12@users.noreply.github.com>
2021-11-19 19:13:37 -0500

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,25 @@
+/**programming assignment 2
+ * Arithmetic.java
+ *  MathOps.java by Ksine library code used 
+ * @author Solomon Tsehay
+ * @since 11/15/2021
+ */
+import java.lang.Math;
+public class Arithmetic {
+ public static void main(String[] args) {
+   
+  //adding numbers
+  System.out.println(MathOps.add(1, 2));
+  
+  //subtract numbers
+  System.out.println(MathOps.subtract(2, 0));
+  
+  //multiply numbers
+  System.out.println(MathOps.multiply(2, 0));
+  
+  //divide numbers
+  System.out.println(MathOps.divide(2, 0));
+ }
+
+}
+
